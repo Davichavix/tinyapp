51616904235a0536eab1c6138c9a2bb7a32bd036
David Chan <dchan902@gmail.com>
2021-11-14 21:59:33 -0700

    implement redirect to shortURL

--- express_server.js
+++ express_server.js
@@ -6,11 +6,11 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 function generateRandomString() {
-  let randStr = ""
+  let randStr = "";
   const randChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
   for (let i = 0; i < 6; i++) {
     randStr = randStr + randChars[Math.floor(Math.random() * 62) + 1];
@@ -22,8 +22,10 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/:${shortURL}`)
 })
 
 app.get("/urls", (req, res) => {
@@ -38,7 +40,7 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { 
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL].longURL
+    longURL: urlDatabase[req.params.shortURL],
   };
   res.render("urls_show", templateVars);
 });
