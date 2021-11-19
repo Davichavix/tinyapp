f3d0f77915be5372f3b3eb5735857d63697d6324
David Chan <dchan902@gmail.com>
2021-11-18 19:49:42 -0700

    added logout to clear cookies

--- express_server.js
+++ express_server.js
@@ -44,6 +44,11 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 })
 
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
+})
+
 app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.newURL;
   res.redirect("/urls")
